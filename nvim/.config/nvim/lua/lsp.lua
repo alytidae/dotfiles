089434cac2178ce48768a47249339d3243e2ac7b
Igor Botukhov <igor.botukhov@gmail.com>
2023-03-30 20:42:09 +0200

    nvim: change

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -45,37 +45,21 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-        -- Use Tab and shift-Tab to navigate autocomplete menu
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-        end,
-        ['<CR>'] = cmp.mapping.confirm {
+		['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
 		['<C-Space>'] = cmp.mapping.complete(),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'snippy' }, -- For snippy users.
+    { name = "nvim_lsp" },
+    { name = "luasnip"  },
   }, {
-    { name = 'buffer' },
+    { name = "buffer" },
+  }, {
+    { name = "path" }
   }),
   completion = {
       autocomplete = false
--- nvim/.config/nvim/lua/plug.lua
+++ nvim/.config/nvim/lua/plug.lua
@@ -25,10 +25,14 @@ return require('packer').startup(function(use)
 	-- CMP
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
 
 	--snippet engine
 	use 'L3MON4D3/LuaSnip'
 	use 'saadparwaiz1/cmp_luasnip'
+
+	use 'simrat39/rust-tools.nvim'
 
 	-- snippet engine
 	-- use 'dcampos/nvim-snippy'
