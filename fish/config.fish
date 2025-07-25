a746455c02cf06c7536d7356f7d35da0583424e3
Igor Botukhov <igor.botukhov@gmail.com>
2025-07-25 19:02:15 +0200

    Added bar

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 set -gx PATH $HOME/.cargo/bin $PATH
 
 if test -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY"
-    exec dbus-run-session -- dwl -s "$HOME/ws/dotfiles/dwl/autostart.sh" <&-
+    ~/ws/dotfiles/dwl/dwl-bar.sh | dbus-run-session -- dwl -s "$HOME/ws/dotfiles/dwl/autostart.sh" <&-
 end
