23f49612675683e1a02422d72aa87e57212554a1
Igor Botukhov <igor.botukhov@gmail.com>
2025-07-25 18:35:14 +0200

    Correct autostart

---
+++ dwl/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+swaybg -i ~/Downloads/wp6405945-2135845896.png &
+waybar &
+udiskie --automount &
+blueman-applet &
+pipewire & disown
+pipewire-pulse & disown
+kanata -c "$HOME/ws/dotfiles/kanata/kanata.kbd" &
+
+
