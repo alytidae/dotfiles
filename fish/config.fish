fc18cf4029a711c80ab2d08b067ef41b19d7bc6a
Igor Botukhov <igor.botukhov@gmail.com>
2025-07-28 10:53:03 +0200

    Added vars to screenshare

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set -gx PATH $HOME/.cargo/bin $PATH
+set -x XDG_CURRENT_DESKTOP wlroots
+set -x XDG_SESSION_TYPE wayland
+set -x XDG_SESSION_DESKTOP wlroots
 
 if test -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY"
-    ~/ws/dotfiles/dwl/dwl-bar.sh | dbus-run-session -- dwl -s "$HOME/ws/dotfiles/dwl/autostart.sh" <&-
+    dbus-run-session -- dwl -s "$HOME/ws/dotfiles/autostart.sh" <&-
 end
