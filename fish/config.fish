3079ff162b996efc6ab77ba5290ecf4ab23549aa
Igor Botukhov <igor.botukhov@gmail.com>
2025-10-13 19:27:08 +0200

    Changed kde lauch

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,12 @@
 set -gx PATH $HOME/.cargo/bin $PATH
-set -x XDG_CURRENT_DESKTOP wlroots
-set -x XDG_SESSION_TYPE wayland
-set -x XDG_SESSION_DESKTOP wlroots
+# set -x XDG_CURRENT_DESKTOP wlroots
+# set -x XDG_SESSION_TYPE wayland
+# set -x XDG_SESSION_DESKTOP wlroots
 
-if test -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY"
-    dbus-run-session -- dwl -s "$HOME/ws/dotfiles/autostart.sh" <&-
+if test (tty) = /dev/tty1
+    exec dbus-run-session startplasma-wayland
 end
+
+# if test -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY"
+# dbus-run-session -- dwl -s "$HOME/ws/dotfiles/autostart.sh" <&-
+# end
