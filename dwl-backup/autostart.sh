2a93930c96fa6579365337a72e24aecbc1c4e834
Igor Botukhov <igor.botukhov@gmail.com>
2026-06-01 20:56:26 +0200

    Removing all tools that I didnt use for a long time now

--- dwl-backup/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-swaybg -i ~/Downloads/wp6405945-2135845896.png &
-udiskie --automount &
-blueman-applet &
-pipewire &
-pipewire-pulse &
-kanata -c "$HOME/ws/dotfiles/kanata/kanata.kbd" &
--- waybar/kblayout.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-layout=$(head -n1 /tmp/dwl-keymap | cut -c1-2 | tr '[:upper:]' '[:lower:]')
-echo "{\"text\": \"$layout\"}"
-
