a1a85f8c9aec11b8d17451aac2c376bb35eba4e7
Igor Botukhov <igor.botukhov@gmail.com>
2026-07-12 16:35:02 +0200

    Prepared for stow

--- fish/config.fish
+++
@@ -1,8 +0,0 @@
-set -gx PATH $HOME/.cargo/bin $PATH
-set -x XDG_CURRENT_DESKTOP wlroots
-set -x XDG_SESSION_TYPE wayland
-set -x XDG_SESSION_DESKTOP wlroots
-
-if test -z "$WAYLAND_DISPLAY" -a -z "$DISPLAY"
-    dbus-run-session -- dwl -s "$HOME/ws/dotfiles/autostart.sh" <&-
-end
